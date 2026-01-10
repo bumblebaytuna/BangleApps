39dca94b32c59eb39e1c9b65b36b0503109a59fb
bumblebaytuna <253105796+bumblebaytuna@users.noreply.github.com>
2026-01-10 17:35:21 +0000

    Update settings.js

--- apps/hourangle/settings.js
+++ apps/hourangle/settings.js
@@ -1,38 +1,47 @@
 (function(back) {
-var SETTINGS_FILE = "hourangle.settings.json";
   
-// Load existing settings
-var mysettings = Object.assign({
-  //set the default values if empty
-  useGPS: false,  // Default GPS setting
-  longitudeDegrees: 3,  // Default longitude angle
-  longitudeDirection: 1,  // Default longitude direction (East=0, West=1)
-  reticuleValidityYearStart: 2000,  // Default start year
-  reticuleValidityYearEnd: 2030,  // Default end year
-  reticuleDisplayStyle: 1  // Default reticule style
-}, require('Storage').readJSON(SETTINGS_FILE, true) || {});
-
-// Write new settings function
-function writeSettings() {require('Storage').writeJSON(SETTINGS_FILE, mysettings);}
-
-
-// Create and show the top-level settings menu
-E.showMenu({
+  var SETTINGS_FILE = "hourangle.settings.json";
+    
+  // Load existing settings
+  var mysettings = Object.assign({
+    //set the default values if empty
+    useGPS: false,  // Default GPS setting
+    longitudeDegrees: 3,  // Default longitude angle
+    longitudeDirection: 1,  // Default longitude direction (East=0, West=1)
+    reticuleValidityYearStart: 2000,  // Default start year
+    reticuleValidityYearEnd: 2030,  // Default end year
+    reticuleDisplayStyle: 1  // Default reticule style
+  }, require('Storage').readJSON(SETTINGS_FILE, true) || {});
   
-  //Add Generic Title and Back button
-  "" : { "title" : "Hour Angle" },
-  "< Back" : () => back(),
+  // Write new settings function
+  function writeSettings() {require('Storage').writeJSON(SETTINGS_FILE, mysettings);}
   
-  // Add Custom Rows
-  'Use GPS': {value: !!mysettings.useGPS, // The !! converts the empty value to the default GPS setting
-  onchange: v => {mysettings.useGPS = v;writeSettings();}},
-  
-  'Display Style': {value: 1|mysettings.reticuleDisplayStyle, // The 1| converts the empty value to the default Display Style setting
-  min: 1,
-  step: 1,
-  max: 2,
-  onchange: v => {mysettings.reticuleDisplayStyle = v; writeSettings(); }},
-});
+    // Create and show the top-level settings menu
+  E.showMenu({
+    
+    //Menu Title and Back button
+    "" : { "title" : "Hour Angle" },
+    "< Back" : () => back(),
+    
+    // Add Custom Rows
+    'Use GPS': {
+      value: !!mysettings.useGPS, // The !! converts the empty value to the default GPS setting
+      onchange: v => {mysettings.useGPS = v;writeSettings();}
+    },
+    
+    'Display Style': {
+      value: 1|mysettings.reticuleDisplayStyle, // The 1| converts the empty value to the default Display Style setting
+      min: 1,
+      step: 1,
+      max: 2,
+      onchange: v => {mysettings.reticuleDisplayStyle = v; writeSettings();}
+    },
+    
+  // showMenu brackets
+  });
 
-// end brackets
+// settings function end brackets
 })
+
+// add this if testing using the Espruino WEB IDE. comment it out if using it for the actual app
+//(load)
