12b4f168df8b1b4c7cf4a1d46865b3d23d3daa79
bumblebaytuna <253105796+bumblebaytuna@users.noreply.github.com>
2026-01-10 17:24:53 +0000

    Update settings.js

--- apps/hourangle/settings.js
+++ apps/hourangle/settings.js
@@ -2,8 +2,9 @@
 var SETTINGS_FILE = "hourangle.settings.json";
   
 // Load existing settings
-var settings = Object.assign({
+var mysettings = Object.assign({
   //set the default values if empty
+  useGPS: false,  // Default GPS setting
   longitudeDegrees: 3,  // Default longitude angle
   longitudeDirection: 1,  // Default longitude direction (East=0, West=1)
   reticuleValidityYearStart: 2000,  // Default start year
@@ -12,20 +13,23 @@ var settings = Object.assign({
 }, require('Storage').readJSON(SETTINGS_FILE, true) || {});
 
 // Write new settings function
-function writeSettings() {require('Storage').writeJSON(SETTINGS_FILE, settings);}
+function writeSettings() {require('Storage').writeJSON(SETTINGS_FILE, mysettings);}
 
 
 // Create and show the settings menu
 E.showMenu({
+  //generic title and back button
   "" : { "title" : "App Name" },
   "< Back" : () => back(),
-  'On or off?': {value: !!settings.onoroff, // !! converts undefined to false
-  onchange: v => {settings.onoroff = v;writeSettings();}},
+  // custom rows
+  'Use GPS': {value: !!mysettings.useGPS, // The !! converts the empty value to the default GPS setting
+  onchange: v => {mysettings.useGPS = v;writeSettings();}},
   
-  'How Many?': {value: 0|settings.howmany, // 0| converts undefined to 0
-  min: 0,
-  max: 10,
-  onchange: v => {settings.howmany = v; writeSettings(); }},
+  'Display Style': {value: 1|mysettings.reticuleDisplayStyle, // The 1| converts the empty value to the default Display Style setting
+  min: 1,
+  step: 1,
+  max: 2,
+  onchange: v => {mysettings.reticuleDisplayStyle = v; writeSettings(); }},
 });
 
 // end brackets
